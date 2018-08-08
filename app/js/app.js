c3e4549d7d4db6fa619045ec6faae767f1c1512f
Joseph Kihiu <Joseph.Kihiu@nicgroup.com>
2018-08-08 19:33:50 +0300

    Init a basic angularjs app with ui-router

---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+
+(function (app) {
+  'use strict';
+
+  // Routing 
+  app.config(function ($stateProvider) {
+
+    let partial1 = {
+      name: "partial1",
+      url: "/partial1",
+      templateUrl: "./partials/partial1.html"
+    }
+
+    let partial2 = {
+      name: "partial2",
+      url: "/partial2",
+      templateUrl: "./partials/partial2.html"
+    }
+    
+    $stateProvider.state(partial1);
+    $stateProvider.state(partial2);
+  })
+  
+ 
+})(angular.module("myApp", ['ui.router','myApp.filters','myApp.services','myApp.directives','myApp.controllers']))
+
+
+
+
