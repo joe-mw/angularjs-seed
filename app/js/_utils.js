f2e0af093fe298feec981f7b14f15292bff76ba6
Joseph Kihiu <Joseph.Kihiu@nicgroup.com>
2018-08-10 18:17:12 +0300

    make divs movable

--- app/js/_utils.js
+++ app/js/_utils.js
@@ -1,13 +1,5 @@
 $(function () {
     
     // loop through the original items
-    $("#original_items span").each(function () {
-        
-        // clone the original items to make their absolute-position counterparts
-        let item = $(this);
-        let item_clone = item.clone();
-
-        // store the clone for later use
-        item.data("clone", item_clone);
-    })
+    $("span").draggable();
 })
